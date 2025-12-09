e3a5ab3863f541618f34b1d3009ab4dd813d557e
BoltPage Developer <noreply@boltpage.dev>
2025-12-09 14:50:45 -0500

    cleaning up

--- boltpage/debug_themes.rs
+++
@@ -1,9 +0,0 @@
-use syntect::highlighting::ThemeSet;
-
-fn main() {
-    let theme_set = ThemeSet::load_defaults();
-    println!("Available themes:");
-    for (name, _) in &theme_set.themes {
-        println!("  - {}", name);
-    }
-}
